d2b12f93ec7205f6a442c9ad550a02ff4abc8325
Lairlynx <vladut.lazar@student.unitbv.ro>
2023-11-22 16:37:07 +0200

    initial

---
+++ src/main/java/com/unitbv/iotmanageappp/IoTManagementSystemAsAWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.unitbv.iotmanageappp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTManagementSystemAsAWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoTManagementSystemAsAWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unitbv/iotmanageappp/IoTManagementSystemAsAWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unitbv.iotmanageappp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoTManagementSystemAsAWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
